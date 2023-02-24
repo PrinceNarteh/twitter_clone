4c64fcce12e45cfb1f0bcdfb7a4ced4eebfe64b0
Prince Narteh <prinarteh@gmail.com>
2023-02-24 01:29:13 +0000

    add appBar

---
+++ lib/constants/ui_constants.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:twitter_clone/constants/constants.dart';
+import 'package:twitter_clone/theme/palette.dart';
+
+class UIConstants {
+  static AppBar appBar() {
+    return AppBar(
+      title: SvgPicture.asset(
+        AssetsConstants.twitterLogo,
+        height: 30,
+        colorFilter: const ColorFilter.mode(
+          Palette.blueColor,
+          BlendMode.srcIn,
+        ),
+      ),
+      centerTitle: true,
+    );
+  }
+}
