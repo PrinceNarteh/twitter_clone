cc31388763d7158cfab41fe0fdc44f92c918626a
Prince Narteh <prinarteh@gmail.com>
2023-02-24 16:47:47 +0000

    add core folder

---
+++ lib/apis/auth_api.dart
@@ -0,0 +1,9 @@
+import 'package:appwrite/appwrite.dart';
+import 'package:twitter_clone/core/core.dart';
+
+abstract class IAuthAPI {
+  FutureEither<Account> signUp({
+    required String email,
+    required String password,
+  });
+}
