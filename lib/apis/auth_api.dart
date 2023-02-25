567ac8d398793527a74afbe3fbcab56d7b354e92
Prince Narteh <prinarteh@gmail.com>
2023-02-25 08:17:46 +0000

    add auth api provider

--- lib/apis/auth_api.dart
+++ lib/apis/auth_api.dart
@@ -1,9 +1,43 @@
-import 'package:appwrite/appwrite.dart';
+import 'package:appwrite/appwrite.dart' as service;
+import 'package:appwrite/models.dart' as model;
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:twitter_clone/core/core.dart';
+import 'package:fpdart/fpdart.dart';
+
+final authAPIProvider = Provider((ref) {
+  final account = ref.watch(appwriteAccountProvider);
+  return AuthAPI(account: account);
+});
 
 abstract class IAuthAPI {
-  FutureEither<Account> signUp({
+  FutureEither<model.Account> signUp({
     required String email,
     required String password,
   });
+}
+
+class AuthAPI implements IAuthAPI {
+  final service.Account _account;
+  AuthAPI({required service.Account account}) : _account = account;
+
+  @override
+  FutureEither<model.Account> signUp({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      final account = await _account.create(
+        userId: service.ID.unique(),
+        email: email,
+        password: password,
+      );
+      return right(account);
+    } on service.AppwriteException catch (e, stackTrace) {
+      return left(
+        Failure(e.message ?? "Some unexpected error occurred", stackTrace),
+      );
+    } catch (e, stackTrace) {
+      return left(Failure(e.toString(), stackTrace));
+    }
+  }
 }
